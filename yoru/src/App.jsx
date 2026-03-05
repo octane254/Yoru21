ec84bd7475a3283804790d1533ce2d88ff514267
Eric Mbithi <mbithieri@gmail.com>
2026-03-05 12:17:04 +0300

    Project Setup

---
+++ yoru/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router"
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+    
+      <Routes>
+        
+      </Routes>
+    
+    </BrowserRouter>
+  )
+}
+
+export default App
